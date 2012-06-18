e566266f8e4c25f023cde51fcc12ce2f35ef6ff0
Boris Vasilyev <vasilyev.boris@gmail.com>
2012-06-18 16:07:38 +0400

    sql

--- scripts/create_db.sql
+++ scripts/create_db.sql
@@ -1,3 +1,5 @@
+create database ems_db;
+
 connect ems_db;
 
 create table employees
@@ -5,3 +7,24 @@ create table employees
 	id mediumint AUTO_INCREMENT,
 	primary key(id)
 );
+
+create table vacancies
+(
+	id mediumint AUTO_INCREMENT,
+	title varchar(500),
+	description varchar(10000),
+	low_salary double,
+	high_salary double,
+	primary key(id)
+);
+
+create table news
+(
+	id mediumint AUTO_INCREMENT,
+	title varchar(500),
+	text varchar(10000),
+	author_id int,
+	publication_date datetime,
+	primary key(id)
+);
+
